24dbff8995fa82854935b8d4c27276ee797e3be4
yn14 <yn14@LAPTOP-BJ8G6G0U>
2018-12-17 14:24:02 +0900

    hello

---
+++ Test2/src/CalcuratorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Test;
+import org.junit.Test;
+public class CalcuratorTest {
+
+	@Test
+	public void testSum() {
+		Calcurator c = new Calcurator();
+		double result = c.sum(10, 50);
+		assertEquals(60,result, 0);
+		
+	}
+
+}
